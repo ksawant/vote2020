99bc2d244aa2f52e58f9da6940a5a1cd5f46eb59
Robin Stewart New <Robin@Robins-MacBook-Pro.local>
2020-02-27 23:17:53 -0800

    removing api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Http, RequestOptions} from "@angular/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private http: Http) {}
+
+  title: string;
+  ngOnInit() {
+    this.title = '2020 Election Information';
+  }
+
+  findStateByZipCode(event: any) {
+    const zipCode = event.target.value;
+    let clientKey = "uAgblRbg9yxAgO6m2GGVh7KvHdVkydYzajK5MY15pCHlAXPxwlWpCJ74cmekefda";
+    const cache = {};
+    console.log(zipCode);
+    if (zipCode.length === 5 && /^[0-9]+$/.test(zipCode)) {
+      if (zipCode in cache) {
+        return cache[zipCode];
+      } else {
+        const url = "https://www.zipcodeapi.com/rest/" + clientKey + "/info.json/" + zipCode + "/radians";
+        this.http.get(url)
+          .toPromise()
+          .then(data => {
+            cache[zipCode] = data;
+            alert(data);
+          })
+          .catch(error => {
+            if(error.responseText) {
+              cache[zipCode] = error.responseText;
+              if (error.responseText.error_msg) {
+                //	errorDiv.text(json.error_msg);
+              } else {
+                //errorDiv.text('Request failed.');
+              }
+            }
+          });
+      }
+    }
+  }
 }
